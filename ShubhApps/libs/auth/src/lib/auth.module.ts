20ecd75bbda9b24c40c488bdfd9f76bb959ea45d
Saurabh jasiwal <62091292+SaurabhxJaiswal@users.noreply.github.com>
2023-04-08 14:49:00 +0530

    Fix error in library code

--- ShubhApps/libs/auth/src/lib/auth.module.ts
+++ ShubhApps/libs/auth/src/lib/auth.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterModule, Route } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { authRoutes } from './lib.routes';
-import { LoginComponent } from './containers/login/login/login.component';
-import { LoginFormComponent } from './components/login-form/login-form/login-form.component';
+import { LoginComponent } from './containers/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(authRoutes), RouterModule],
