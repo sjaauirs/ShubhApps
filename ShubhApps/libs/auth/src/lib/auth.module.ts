2ef96778e8a5a145c86859cc697eaae26faf54da
Saurabh jasiwal <62091292+SaurabhxJaiswal@users.noreply.github.com>
2023-04-08 13:15:48 +0530

    Add Component in Auth Library

--- ShubhApps/libs/auth/src/lib/auth.module.ts
+++ ShubhApps/libs/auth/src/lib/auth.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterModule, Route } from '@angular/router';
 import { authRoutes } from './lib.routes';
+import { LoginComponent } from './containers/login/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form/login-form.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(authRoutes), RouterModule],
+  declarations: [LoginComponent, LoginFormComponent],
 })
 export class AuthModule {}
---
+++ ShubhApps/libs/auth/src/lib/components/login-form/login-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shubh-apps-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss'],
+})
+export class LoginFormComponent {}
