93dda9e45a5c1438cd3e320ef75e23aa201dba9a
Saurabh jasiwal <62091292+SaurabhxJaiswal@users.noreply.github.com>
2023-04-10 03:33:41 +0530

    Fix Route fot loginComponent in remote Customer portal

--- ShubhApps/apps/customer-portal/src/app/app.routes.ts
+++ ShubhApps/apps/customer-portal/src/app/app.routes.ts
@@ -6,4 +6,10 @@ export const appRoutes: Route[] = [
     loadChildren: () =>
       import('./remote-entry/entry.module').then((m) => m.RemoteEntryModule),
   },
+
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@shubh-apps/auth').then((m) => m.AuthModule),
+  },
 ];
--- ShubhApps/libs/auth/src/lib/lib.routes.ts
+++ ShubhApps/libs/auth/src/lib/lib.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginComponent } from './containers/login/login.component';
 
-export const authRoutes: Route[] = [];
+export const authRoutes: Route[] = [
+  { path: 'login', component: LoginComponent },
+  { path: 'loginForm', component: LoginFormComponent }
+
+];
